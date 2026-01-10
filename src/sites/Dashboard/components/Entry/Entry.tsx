333e228a4b678f5ca67259ac22ef8dd29e2c6abc
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-10 20:58:30 +0100

    calculate time differences

--- src/sites/Dashboard/components/Entry/Entry.tsx
+++ src/sites/Dashboard/components/Entry/Entry.tsx
@@ -9,12 +9,26 @@ type Props = {
 export default function Entry({ entry, entries, currentTime } : Props) {
     const timestamp : number = entries[entry].timestamp;
 
-    const timeDifference = Math.floor((currentTime - timestamp) / 1000); // <- difference in seconds
+    const timeDifference = (currentTime - timestamp) / 1000; // <- difference in seconds
+
+    const timeDifferences: Map<string, number> = new Map([
+        ["seconds", Math.floor((timeDifference) % 60)],
+        ["minutes", Math.floor((timeDifference / 60) % 60)],
+        ["hours", Math.floor((timeDifference / (60 * 60)) % 24)],
+        ["days", Math.floor((timeDifference / (60 * 60 * 24)) % 30.4583)],
+        ["months", Math.floor((timeDifference / (60 * 60 * 24 * 30.4583) % 12))],
+        ["years", Math.floor(timeDifference / (60 * 60 * 24 * 30.4583 * 12))]
+    ]);
 
     return (
         <div className="entry">
             <h2>{entry}</h2>
-            <p>{timeDifference >= 0 ? timeDifference : 0} seconds</p>
+            <p>{timeDifferences.get("years")} years</p>
+            <p>{timeDifferences.get("months")} months</p>
+            <p>{timeDifferences.get("days")} days</p>
+            <p>{timeDifferences.get("hours")} hours</p>
+            <p>{timeDifferences.get("minutes")} minutes</p>
+            <p>{timeDifferences.get("seconds")} seconds</p>
         </div>
     )
 }
