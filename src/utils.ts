2f11d7a2de0f35955b5cfda2dc688e3445a3f282
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-10 18:07:10 +0100

    general improvements

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,20 @@
-export function addEntry(name: string) {
+export function addEntry(name: string, setEntries: Function) {
     const entries = getEntries();
 
     if (entries[name]) {
         throw new Error(`An entry with the name "${name}" already exists`);        
     } else {
         entries[name] = {timestamp: Date.now()};
-        localStorage.setItem("entries", JSON.stringify(entries));
+        setEntries(entries);
     }
 }
 
-export function removeEntry(name: string) {
+export function removeEntry(name: string, setEntries: Function) {
     const entries = getEntries();
 
     if (entries[name]) {
         delete entries[name];
-        localStorage.setItem("entries", JSON.stringify(entries));
+        setEntries(entries);
     } else {
         throw new Error(`An entry with the name ${name} doesn't exist`);
     }
