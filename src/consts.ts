1d25a94717dd81dc3c943b0242bd7bf285bac28a
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-11 18:08:30 +0100

    save accent color as const

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,1 @@
-export const style: CSSStyleDeclaration = window.getComputedStyle(document.body);
-
-export const accentColor: string = style.getPropertyValue("--accent");
+export const accentColor: string = "#000000";
