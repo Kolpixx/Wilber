74326a5411e77e493550f65e53c3270cf27a725d
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-25 22:33:50 +0100

    use class names instead of id's

--- src/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -9,11 +9,11 @@ type Props = {
 export default function ConfirmationModal({ showModal, text, confirmFunction } : Props) {
     return (
         <div className="modal-wrapper" onClick={(e) => {(e.target as HTMLElement).classList[0] === "modal-wrapper" && showModal(false)}}>
-            <div className="modal" id="confirmation-modal">
+            <div className="modal confirmation-modal">
                 <span>{text}</span>
-                <div id="confirmation-buttons">
-                    <button id="confirmation-buttons-abort" className="pointer" onClick={() => showModal(false)}>Abort</button>
-                    <button id="confirmation-buttons-confirm" className="pointer" onClick={() => {confirmFunction(); showModal(false)}}>Confirm</button>
+                <div className="confirmation-buttons">
+                    <button className="confirmation-buttons-abort pointer" onClick={() => showModal(false)}>Abort</button>
+                    <button className="confirmation-buttons-confirm pointer" onClick={() => {confirmFunction(); showModal(false)}}>Confirm</button>
                 </div>
             </div>
         </div>
