5d5b3cf3fd74113aa57b951f45731f0cb53f7897
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-25 17:07:38 +0100

    style confirmation modal

--- src/components/ConfirmationModal/ConfirmationModal.tsx
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -1,3 +1,5 @@
+import './ConfirmationModal.css'
+
 type Props = {
     showModal: Function,
     text: string,
@@ -7,11 +9,11 @@ type Props = {
 export default function ConfirmationModal({ showModal, text, confirmFunction } : Props) {
     return (
         <div className="modal-wrapper" onClick={(e) => {(e.target as HTMLElement).classList[0] === "modal-wrapper" && showModal(false)}}>
-            <div className="modal">
-                <div>{text}</div>
+            <div className="modal" id="confirmation-modal">
+                <span>{text}</span>
                 <div id="confirmation-buttons">
-                    <button onClick={() => {confirmFunction(); showModal(false)}}>Confirm</button>
-                    <button onClick={() => showModal(false)}>Abort</button>
+                    <button id="confirmation-buttons-abort" className="pointer" onClick={() => showModal(false)}>Abort</button>
+                    <button id="confirmation-buttons-confirm" className="pointer" onClick={() => {confirmFunction(); showModal(false)}}>Confirm</button>
                 </div>
             </div>
         </div>
