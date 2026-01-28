562bb1d6838feb5a1146c2ecb6a48aa02f9cc78f
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-28 19:31:31 +0100

    add Rosé Pine theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const themes = [["system", "System"], ["light", "Light"], ["dark", "Dark"], ["catppuccin-mocha", "Catppuccin"]] as const;
+export const themes = [["system", "System"], ["light", "Light"], ["dark", "Dark"], ["catppuccin-mocha", "Catppuccin"], ["rose-pine", "Rosé Pine"]] as const;
 export const themeMap = new Map(themes);
 
 export type Theme = typeof themes[number][0];
